a237d4435d2c74bf7e5d3739aed3c0514c196729
rreone2017 <rreone2017@gmail.com>
2022-07-12 15:02:23 +0900

    make Map<> in Rq class

--- src/main/java/package1/Rq.java
+++ src/main/java/package1/Rq.java
@@ -1,50 +1,67 @@
 package package1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
 
     String url;
     String path;
-    String queryStr;
+
+    Map<String, String> queryParams;
 
     public Rq(String url) {
+
         this.url = url;
         String[] urlBits = url.split("\\?", 2);
         this.path = urlBits[0];
 
+
+        queryParams = new HashMap<>();
+
+
         if(urlBits.length == 2){
-            this.queryStr = urlBits[1];
+            String queryStr = urlBits[1];
+
+            String[] paramBits = queryStr.split("&");
+
+            for(String paramBit : paramBits) {
+
+                String[] paramNameAndValue = paramBit.split("=", 2);
+
+                if(paramNameAndValue.length == 1){
+                    continue;
+                }
+
+
+                String paramName = paramNameAndValue[0].trim();
+                String paramValue = paramNameAndValue[1].trim();
+
+                queryParams.put(paramName, paramValue);
+            }
+
+
         }
 
     }
 
     public int getIntParam(String inputParamName, int defaultValue) {
 
-        if(queryStr == null){
+        if(queryParams.containsKey(inputParamName) == false){
             return defaultValue;
         }
 
-        String[] bits = queryStr.split("&");
+        String paramValue = queryParams.get(inputParamName);
 
-        for(String urlBit : bits) {
-
-            String[] paramNameAndValue = urlBit.split("=", 2);
-            String paramName = paramNameAndValue[0];
-            String paramValue = paramNameAndValue[1];
-
-
-            if(inputParamName.equals(paramName)) {
-                return Integer.parseInt(paramValue);
-            }
+        if(paramValue.length() == 0){
+            return defaultValue;
         }
 
-        return defaultValue;
-
+        return Integer.parseInt(paramValue);
     }
 
     public String getPath() {
-
         return path;
     }
-
 
 }
