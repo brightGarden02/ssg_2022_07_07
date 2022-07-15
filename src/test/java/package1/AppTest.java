5d5c66d86bf5db37e8e870b47c239ad3e74e1da8
rreone2017 <rreone2017@gmail.com>
2022-07-15 16:41:21 +0900

    increase wiseSaying number when write() is executed

--- src/test/java/package1/AppTest.java
+++ src/test/java/package1/AppTest.java
@@ -12,6 +12,27 @@ public class AppTest {
 
 
     @Test
+    public void 등록을_하면_생성된_명언의_번호가_출력되어야_한다() {
+
+        String rs = AppTestRunner.run("""
+                등록
+                나의 죽음을 적들에게 알리지 말라
+                이순신
+                등록
+                나에게 불가능이란 없다.
+                나폴레옹
+                종료
+                """);
+
+
+        assertTrue(rs.contains("1번 명언이 등록되었습니다."));
+        assertTrue(rs.contains("2번 명언이 등록되었습니다."));
+
+    }
+
+
+
+    @Test
     public void 등록을_하면_명언과_작가를_물어본다() {
 
         String rs = AppTestRunner.run("""
